c56f5144e28078e6312eae4c4851accbdd74776b
Faboo <fabienleroy2004@gmail.com>
2024-03-04 14:41:03 +0100

    bug

--- controle-qualite.bat
+++ controle-qualite.bat
@@ -1,4 +1,4 @@
-SET MAVEN_HOME=c:\Data\maven
+SET MAVEN_HOME=C:\Program Files\apache-maven-3.8.5
 SET JAVA_HOME=C:\Program Files\Java\jdk-11.0.11
 SET PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%PATH%
 REM Lancer à la racine du projet
